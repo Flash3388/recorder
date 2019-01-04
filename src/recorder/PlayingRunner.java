e7824911b27e046e0fe09f1f9a9ef09049b19e85
DanielM3388 <israelibit@gmail.com>
2019-01-04 19:23:44 +0200

    Removed unnecessary constants

--- src/recorder/PlayingRunner.java
+++ src/recorder/PlayingRunner.java
@@ -1,17 +1,11 @@
 package recorder;
 
-public class PlayingRunner{
-	private final static int DEFAULT_PERIOD_MS = 10;
-	
+public class PlayingRunner{	
 	private Executor mExecutor;
 	private String mOutputPath;
 	
 	private Player[] mPlayers;
 	private int mPeriodMs;
-	
-	public PlayingRunner(String outputPath, Player... players) {
-		this(outputPath,DEFAULT_PERIOD_MS,players);
-    }
 	
 	/**
 	 * 
--- src/recorder/RecordingRunner.java
+++ src/recorder/RecordingRunner.java
@@ -1,19 +1,11 @@
 package recorder;
 
 public class RecordingRunner {
-
-	public static final String FILE_PATH = "/home/lvuser/recorder/";
-	private final static int DEFAULT_PERIOD_MS = 10;
-	
     private Recorder[] mRecorders;
     private Executor mExecutor;
 
     private String mOutputPath;
     private int mPeriodMs;
-    
-    public RecordingRunner(String fullPath, Recorder... recorders) {
-        this(fullPath,DEFAULT_PERIOD_MS,recorders);
-    }
     
     public RecordingRunner(String fullPath, int periodMs, Recorder... recorders) {
     	if(periodMs <= 0 )
