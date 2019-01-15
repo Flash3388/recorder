06b56c58e938bb3632ca818d90beb03dea76376b
DanielM3388 <israelibit@gmail.com>
2019-01-15 14:59:11 +0200

    Playing task can now be stopped by an exception

--- src/recorder/PlayingTask.java
+++ src/recorder/PlayingTask.java
@@ -3,6 +3,7 @@ package recorder;
 import edu.flash3388.flashlib.util.FlashUtil;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayDeque;
@@ -26,8 +27,14 @@ public class PlayingTask implements Runnable {
 
     @Override
     public void run() {
-    	Queue<Frame> frames = loadFrames(mInputPath);
+    	Queue<Frame> frames = null;
         long startTime = FlashUtil.millisInt();
+        
+        try {
+			frames = loadFrames(mInputPath);
+		} catch (IOException e) {
+        	mLogger.log(Level.SEVERE,String.format("Error accured while reading frames from %s", mPlayer.getName()),e);  
+        }
         
         if(frames != null) {
         	for(Frame frame : frames) {
@@ -43,17 +50,15 @@ public class PlayingTask implements Runnable {
         }
     }
 
-    private Queue<Frame> loadFrames(String path) {
+    private Queue<Frame> loadFrames(String path) throws FileNotFoundException, IOException {
         Queue<Frame> frames = new ArrayDeque<Frame>();
         
         try(BufferedReader file = new BufferedReader(new FileReader(path))) {
         	for(String line = file.readLine(); line != null; line = file.readLine()) {
             	frames.add(new Frame(line));
             }
-        } catch (IOException e) {
-        	mLogger.log(Level.SEVERE,String.format("Error accured while reading frames from %s", path),e);  
-        	return null;
         }
+        
         return frames;
     }
 }
